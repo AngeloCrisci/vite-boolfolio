552dc8ce764a844a1a4a62cb5780690902e2deef
AngeloCrisci <angelocrisci1998@gmail.com>
2024-11-16 18:44:35 +0100

    Added method axios for getting the api, proceded to full the template with dinamic properites (need to fix an error on the terminal)(esercizio finito)

--- vite-boolfolio/src/components/Appmain.vue
+++ vite-boolfolio/src/components/Appmain.vue
@@ -5,11 +5,31 @@
     export default {
         name: "Appmain",
         data(){
-            return{}
+            return{
+                projectsList : [],
+            }
         },
         methods: {
+            getProjects(){
+                axios.get('http://127.0.0.1:8000/api/projects')
+                .then(response) => {
+                    // handle success
+                    console.log(response.data.results);
 
+                    this.projectsList = response.data.results;
+                }
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                } );
+            }
         },
+        created(){
+            this.getProjects();
+        }
         computed:{
 
         },
@@ -24,11 +44,23 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="card" style="width: 18rem;">     
+                <div class="card col-3" v-for="project in projectsList" :key="project.id" style="width: 18rem;">     
                 <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text"></p>
-                    <a href="#" class="btn btn-primary"></a>
+                    <h5 class="card-title">
+                        {{ project.name }}
+                    </h5>
+                    <p class="card-text">
+                         {{ project.date }}
+                    </p>
+                    <p class="card-text">
+                         {{ project.description }}
+                    </p>
+                    <p class="card-text">
+                         {{ project.date }}
+                    </p>
+                    <a :href="{{ description.link }}" class="btn btn-primary">
+                        Link
+                    </a>
                 </div>
                 </div>
             </div>
