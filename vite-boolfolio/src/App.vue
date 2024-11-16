c2f7600df44373b32d0472147db08429e0bf9c88
AngeloCrisci <angelocrisci1998@gmail.com>
2024-11-16 18:25:56 +0100

    imported bootstrap and scss, added App main and linked to App, created the cards fot the template

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,11 +1,23 @@
-<script setup>
-  import Appmain from ""
+<script>
+  import Appmain from "./components/Appmain.vue";
+
+  export default {
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      Appmain,
+    }
+  }
 </script>
 
 <template>
-  
+  <Appmain/>
 </template>
 
 <style lang="scss">
-  
+  @use "bootstrap/scss/bootstrap.scss" as *;
+  @use "./styles/general.scss" as *;
 </style>
--- vite-boolfolio/src/components/Appmain.vue
+++ vite-boolfolio/src/components/Appmain.vue
@@ -18,7 +18,22 @@
 </script>
 
 <template>
-  
+    <h1 class="text-center">
+        MY PROJECTS
+    </h1>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="card" style="width: 18rem;">     
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary"></a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped>
